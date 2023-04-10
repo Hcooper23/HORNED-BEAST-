8f31edd60dc0833a64c360cb61c28304485c7fe2
Hayden James Cooper <haydencpr95@gmail.com>
2023-04-10 19:16:58 -0400

    Lab Class 01

---
+++ src/components/hornedbeast.js
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+
+class HornedBeast extends Component {
+  render() {
+    return (
+      <>
+        <h2>{this.props.title}</h2>
+        <img src={this.props.image_url} alt="text" title="title"></img>
+        <p> {this.props.description}</p>
+      </>
+    )
+  }
+}
+export default HornedBeast;
---
+++ src/footer.js
@@ -0,0 +1,10 @@
+import { Component } from 'react';
+
+class Footer extends Component{
+    render() {
+      return (
+        <footer>Hayden Cooper</footer>
+      )
+    }
+}
+export default Footer
