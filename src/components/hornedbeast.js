d91c6ed677f124899aa9c796063f2318d8d262eb
Hayden James Cooper <haydencpr95@gmail.com>
2023-04-11 21:26:49 -0400

    Lab Class 02

--- src/components/hornedbeast.js
+++ src/components/hornedbeast.js
@@ -1,14 +1,25 @@
 import { Component } from 'react';
+ import Image from 'react-bootstrap/Image';
+ class HornedBeast extends Component {
+   constructor(props) {
+     super(props);
+     this.state = {
+       likes: 0
+     }
+   }
+   addClick = () => {
+     this.setState({
+       likes: this.state.likes + 1
+     })
+   }
+   render() {
+     return (
 
-class HornedBeast extends Component {
-  render() {
-    return (
-      <>
-        <h2>{this.props.title}</h2>
-        <img src={this.props.image_url} alt="text" title="title"></img>
-        <p> {this.props.description}</p>
-      </>
-    )
-  }
-}
-export default HornedBeast;
+       <div>
+         <Image width="200px" src={this.props.beast.image_url} onClick={this.addClick} />
+         <p>&hearts; {this.state.likes} </p>
+       </div>
+     )
+   }
+ }
+ export default HornedBeast;
